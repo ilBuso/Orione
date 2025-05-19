70f1feb2124990bc8cecf5c0e1f04747a33cdfa5
ilBuso <alessandro.busola@gmail.com>
2025-05-19 16:29:00 +0200

    helper functions

--- driver/src/global/global.h
+++ driver/src/global/global.h
@@ -1,4 +1,14 @@
 #pragma once
 
+#include <stdint.h>
+
 #define NUM_ROWS 6
 #define NUM_COLS 17
+
+inline bool check_pressed_flag(uint8_t info) {
+    return (info & 0x01) != 0;
+}
+
+inline bool check_encoder_flag(uint8_t info) {
+    return (info & 0x02) != 0;
+}
