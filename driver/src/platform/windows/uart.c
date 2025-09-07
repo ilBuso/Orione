e55d925e15ca63c3a60cd156b05faea704ebb3b5
ilBuso <alessandro.busola@gmail.com>
2025-09-07 11:30:05 +0200

    v2

--- driver/src/platform/windows/uart.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by Giulio Pimenoff Verdolin on 08/05/25.
-//
-
-#import "core/io/uart_init.h"
--- firmware/src/matrix/debouncing/debouncing.c
+++
@@ -1,9 +0,0 @@
-#include "debouncing.h"
-
-#include <time.h>
-
-uint32_t key_timestamps[NUM_ROWS][NUM_COLS] = {0};
-
-inline uint32_t get_current_timestamp_ms(void){
-    return (uint32_t)(clock() * 1000 / CLOCKS_PER_SEC);
-}
